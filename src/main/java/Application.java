fdd9aedded1c8dc647ba0c83c63ef16f52937c07
kavitha <kavithasubbiah@hotmail.com>
2021-06-21 14:43:27 -0400

    Setter injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,16 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = ctx.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
