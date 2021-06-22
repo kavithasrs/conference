81414a6c83dc1ff3ec5f701c8bdcc3afb8ad4352
kavitha <kavithasubbiah@hotmail.com>
2021-06-22 09:39:39 -0400

    Autowired

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -3,21 +3,26 @@ import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.pluralsight")
 public class AppConfig {
 
-    @Bean(name = "speakerService")
+    /*@Bean(name = "speakerService")
     public SpeakerService getSpeakerService(){
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        //SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());
         return service;
     }
+
 
     @Bean(name = "speakerRepository")
     public SpeakerRepository getSpeakerRepository(){
         return new HibernateSpeakerRepositoryImpl();
 
     }
+    */
 }
